9f0f7c2ce219a7ad0405cd0fe514783bf5907243
darromero <dromero78@mail.pima.edu>
2024-09-22 21:54:50 -0700

    Prints a story about an alien and his love for a mucisian on cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Dario Romero
+# My first python lab in CIS 129 course
+# Prints strange alien story
+print('''
+Once there was an alien from another galaxy that came to planet earth in order to meet a famous musician
+made famous for his talent on the piano. The alien perplexed by the unusual draww he had towards the young humans recordings
+drive him to fly across the galaxy to hear the musician preform live. When the alien arrived he ended up being dissapointed to find
+the human had passed a long time ago. In honor of his favorite musician the alien returned to his home planet with a new piano. 
+''')
